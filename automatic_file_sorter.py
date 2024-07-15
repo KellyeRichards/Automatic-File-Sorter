342e85c251cdb3fbdf7d628b48ea8f20cdca0273
Kellye Richards <kellye.whitfill@gmail.com>
2024-07-15 15:00:33 -0500

    Initial Commit

---
+++ automatic_file_sorter.py
@@ -0,0 +1,22 @@
+import os, shutil
+
+path = r"/Users/kellye/Documents/"
+
+file_name = os.listdir(path)
+
+folder_names = ['pdf files', 'img files', 'text files']
+
+for loop in range(0,3):
+    if not os.path.exists(path + folder_names[loop]):
+        print(path + folder_names[loop])
+        os.makedirs((path + folder_names[loop]))
+
+for file in file_name:
+    if ".pdf" in file and not os.path.exists(path + "pdf files/" + file):
+        shutil.move(path + file, path + "pdf files/" + file)
+    elif ".jpg" in file and not os.path.exists(path + "img files/" + file):
+        shutil.move(path + file, path + "img files/" + file)
+    elif ".docx" in file and not os.path.exists(path + "text files/" + file):
+        shutil.move(path + file, path + "text files/" + file)
+
+=-
